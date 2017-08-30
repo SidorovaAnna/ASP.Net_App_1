7b3e314d695fe7ae00166078031bd225916f3eab
SidorovaAnna <ann.sidorova@mail.ru>
2017-08-30 22:13:27 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult ProductsList()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadFile()
+        {
+            string filepath = Server.MapPath("/Content/ForDownload.pdf");
+            string contentType = "application/pdf";
+            string downloadName = "PDF File";
+            downloadName = null;
+            return File(filepath, contentType, downloadName);
+        }
+
+      
+    }
+}
